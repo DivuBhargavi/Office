a238e5013f2e849a4e02db08b485bf57b99c30c4
DivuBhargavi <161690665+DivuBhargavi@users.noreply.github.com>
2024-08-05 11:55:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-//Introduction to Express.js: Setting up an Express server. Create basic routes and middleware.
-//  Express.js is a popular web framework for Node.js that makes it easier to build web applications and APIs. Let’s walk through setting up a basic Express server, creating routes, and using middleware.
-import express from "express"; // Import the express module
+
+import express from "express"; 
  
 const port = 3000
 
-const app = express(); // Create an Express app
+const app = express(); 
 
-app.use(express.json()); // middleware to parse JSON request bodies
+app.use(express.json()); 
 
-app.get('/hii', (req,res) =>{ // Define a GET route for /hii
+app.get('/hii', (req,res) =>{ 
     res.send("hello world");
 })
 
-app.listen(port, () => { // Start the server and log a message when it's running
+app.listen(port, () => { 
     console.log(`Server running on port ${port}`);
   });
